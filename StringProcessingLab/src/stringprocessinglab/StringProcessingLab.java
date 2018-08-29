0434ad3c683bb1b99d6342b9ee6708aa3aa43a8e
keshawn1996 <akeshawn@yahoo.com>
2018-08-29 13:33:11 -0400

    Completed levels 2 and 3

--- StringProcessingLab/src/stringprocessinglab/StringProcessingLab.java
+++ StringProcessingLab/src/stringprocessinglab/StringProcessingLab.java
@@ -7,9 +7,6 @@ package stringprocessinglab;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,46 +17,105 @@ import java.util.logging.Logger;
  */
 public class StringProcessingLab {
 
-    
+    public static String[] records;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    String first = null;
-    String last = null;
-    String gender = null;
-    int age = 0;
-    String phone = null;
-    String email = null;
         // TODO code application logic here
         String filename = "records.txt";
         final int MAXRECORDS = 100;
-        String [] records;
+
         records = new String[MAXRECORDS];
         String output = null;
-        readRecords(filename, output);   
+        readRecords(filename, output);
+    }
+
+    public static void readRecords(String filename, String ouput) {
+        try {
+            Scanner reader = new Scanner(new File(filename));
+            int currentIndex = 0;
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                String[] details = line.split(",");
+                String first = details[0];
+                String last = details[1];
+                String gender = details[2];
+                String age = details[3];
+                String phone = details[4];
+                String email = details[5];
+                
+                String output = String.format("%-10s%-10s%-10s%-10s%-10s%30s", first, last, gender, age, phone, email);
+if (nameHasLetters(first, last) && genderIsValid(gender) && ageIsValid(age) && phoneNumberIsValid(phone) && emailIsValid(email)) {
+                    records[currentIndex] = output;
+                } else {
+                    System.err.println(output);
+                }
+                currentIndex++;
+            }
+
+         for (String record : records) {
+                System.out.println(record);
+           }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(StringProcessingLab.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static boolean nameHasLetters(String first, String last) {
+        first = first.trim();
+        last = last.trim();
+        if (first.matches("[a-zA-Z]+")) {
+            if (last.matches("[a-zA-Z]+")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean genderIsValid(String gender) {
+        gender = gender.trim();
+        return gender.equalsIgnoreCase("male") || gender.equalsIgnoreCase("female");
+    }
+
+    public static boolean ageIsValid(String age) {
+            age = age.trim();
+            if(age.equals(""))
+            {
+                return false;
+            }
+            else 
+            {
+            int result = Integer.parseInt(age);
+            return result > 1 && result <= 130;
+           }
+            
+    } 
+    
+    public static boolean phoneNumberIsValid(String phone)
+    {
+        phone = phone.trim();
+      if(phone.matches("(((\\(\\d{3}\\) ?)|(\\d{3}-))?\\d{3}-\\d{4})"))
+      {
+          return true;
+      }
+      else
+      {
+          return false;
+      }
     }
     
-    public static void readRecords(String filename, String ouput) {
-        try 
+    public static boolean emailIsValid(String email)
+    {
+        email = email.trim();
+        if(email.matches("^(.+)@(.+)$"))
         {
-            List<String> record = new ArrayList<>();
-            Scanner reader = new Scanner(new File(filename));
-            while(reader.hasNextLine())
-            {
-                    String line = reader.nextLine();
-                    String[] details = line.split(",");
-                    String first = details[0];
-                    String last = details[1];
-                    String gender = details[2];
-                    String age = details[3];
-                    String phone = details[4];
-                    String email = details[5];
-                String output = String.format("%-10s%-10s%-10s%-10s%-10s%30s", first, last, gender, age, phone, email);
-                System.out.println(output);
-                }
-    }   catch (FileNotFoundException ex) {
-            Logger.getLogger(StringProcessingLab.class.getName()).log(Level.SEVERE, null, ex);
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
